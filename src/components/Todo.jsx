38eb5e8cb1957516de8c67a0a6c5b80ca58b0559
izzett222 <iizzeddin62@gmail.com>
2023-03-15 14:36:54 +0200

    move event handler to todo component

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,32 +1,31 @@
 import trash from "../assets/trash.svg";
 import edit from "../assets/edit.svg";
 import tick from "../assets/tick.svg";
-import { useState } from "react";
-export default function Todo({ done, text, check, remove, updateTask }) {
+import { useState, useContext } from "react";
+import todoContext from "../todoContext";
+
+export default function Todo({ done, text, id }) {
   const [updatedText, setUpdatedText] = useState(text);
   const [update, setUpdate] = useState(false);
+  const { deleteTodo, updateTodoText, completeTodo } = useContext(todoContext);
 
   const handleUpdate = () => {
     setUpdate(true);
   };
   const handleSave = () => {
     if (updatedText.trim() !== text) {
-      updateTask(updatedText);
+      updateTodoText(id, updatedText);
     }
     setUpdate(false);
   };
-  const handleChange = (event) => {
-    const { value } = event.target;
-    setUpdatedText(value);
-  };
+  
   return (
     <div className="w-full flex items-center pb-5 pt-4  border-b border-b-gray-400">
       <input
         type="checkbox"
         name="done"
-        id=""
         checked={done}
-        onChange={check}
+        onChange={() => completeTodo(id)}
       />
       {!update ? (
         <span
@@ -40,7 +39,7 @@ export default function Todo({ done, text, check, remove, updateTask }) {
         <input
           className={`text-lg text-gray-700 block font-semibold w-[60%] mx-3 border rounded-sm p-1 pb-1.5 indent-[10px] shadow-inner`}
           value={updatedText}
-          onChange={handleChange}
+          onChange={(event) => setUpdatedText(event.target.value)}
         />
       )}
 
@@ -63,7 +62,10 @@ export default function Todo({ done, text, check, remove, updateTask }) {
             </button>
           )}
         </div>
-        <button onClick={remove} className="block p-2 bg-red-100 rounded-full">
+        <button
+          onClick={() => deleteTodo(id)}
+          className="block p-2 bg-red-100 rounded-full"
+        >
           <img src={trash} alt="" className="w-5 h-5" />
         </button>
       </div>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,28 +3,11 @@ import { useContext } from "react";
 import todoContext from "../todoContext";
 
 export default function TodoList() {
-  const { todos, deleteTodo, updateTodoText, completeTodo} = useContext(todoContext)
-  const createCompleteHandler = (id) => {
-    return () => completeTodo(id);
-  };
-  const createDeleteHandler = (id) => {
-    return () => deleteTodo(id);
-  };
-  const createUpdateHandler = (id) => {
-    return (text) => updateTodoText(id, text);
-  };
-
+  const { todos } = useContext(todoContext);
   return (
     <div className="w-full mt-5">
       {todos.map((el) => (
-        <Todo
-          key={el.id}
-          remove={createDeleteHandler(el.id)}
-          check={createCompleteHandler(el.id)}
-          updateTask={createUpdateHandler(el.id)}
-          done={el.done}
-          text={el.text}
-        />
+        <Todo key={el.id} {...el} />
       ))}
     </div>
   );
